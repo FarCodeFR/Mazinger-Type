40a2d3b7299e10348bee1f3fc0f40b01fbb7e6a9
FarCodeFR <Timo-44@hotmail.com>
2025-11-20 16:49:07 +0100

    Add port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = 3080;
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "public")));
@@ -10,5 +11,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3080, () => {
-  console.log(`Server is runing`);
+  console.log(`🚀 Server running at http://localhost:${port}`);
 });
