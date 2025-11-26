6d5276ff8e3a6fee0684def400e5546a8f391272
ViriatoF <viriato.ferreira44@gmail.com>
2025-11-26 21:58:58 +0100

    Add small blueprint and player constructor

---
+++ assets/js/player.js
@@ -0,0 +1,12 @@
+class Player {
+  constructor(y, config) {
+    this.x = 0;
+    this.y = y;
+    this.speed = config.speed;
+    this.size = config.size;
+    this.hp = config.hp;
+    this.model = config.model;
+  }
+
+
+}
