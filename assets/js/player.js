418a6c0e72104a4450b783ebd491e6f7f6825757
ViriatoF <viriato.ferreira44@gmail.com>
2025-11-27 15:16:40 +0100

    Right, left, up and down moves runs but just with left and right constrain

--- assets/js/player.js
+++ assets/js/player.js
@@ -8,6 +8,26 @@ class Player {
     this.model = config.model;
   }
 
+  update() {
+    if (keyIsDown(LEFT_ARROW)){
+      this.x -= this.speed;
+    }
+
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += this.speed;
+    }
+
+    if (keyIsDown(UP_ARROW)) {
+      this.y -= this.speed;
+    }
+
+    if (keyIsDown(DOWN_ARROW)) {
+      this.y += this.speed;
+    }
+
+    this.x = constrain(this.x, (-width + this.size) / 2, (width - this.size) / 2, -height / 2, height / 2)
+  }
+
   draw() {
     push();
     translate(this.x, this.y, 20);
